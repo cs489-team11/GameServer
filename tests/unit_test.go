946f312e37a041b1c1f79aaf0a309be642f3abe3
temirrr <temirlanmirzakhmetov@gmail.com>
2020-11-07 21:23:52 +0900

    Add TestStart unit test

--- tests/unit_test.go
+++ tests/unit_test.go
@@ -30,11 +30,11 @@ func TestJoinAndLeave(t *testing.T) {
 	err = client3.Connect(serverAddr)
 	require.NoError(t, err)
 
-	_, err = client1.JoinGame()
+	joinRes1, err := client1.JoinGame()
 	require.NoError(t, err)
-	_, err = client2.JoinGame()
+	joinRes2, err := client2.JoinGame()
 	require.NoError(t, err)
-	_, err = client3.JoinGame()
+	joinRes3, err := client3.JoinGame()
 	require.NoError(t, err)
 
 	err = client2.LeaveGame()
@@ -46,8 +46,13 @@ func TestJoinAndLeave(t *testing.T) {
 	err = client4.Connect(serverAddr)
 	require.NoError(t, err)
 
-	_, err = client4.JoinGame()
+	joinRes4, err := client4.JoinGame()
 	require.NoError(t, err)
+
+	// check that all of the players are joining the same game
+	require.Equal(t, joinRes1.GameId, joinRes2.GameId)
+	require.Equal(t, joinRes2.GameId, joinRes3.GameId)
+	require.Equal(t, joinRes3.GameId, joinRes4.GameId)
 
 	// TODO: check that there is only
 	// client1, client4, and bank returned
@@ -63,6 +68,42 @@ func TestJoinAndLeave(t *testing.T) {
 	// returns no error
 	err = client1.LeaveGame()
 	require.NoError(t, err)
+}
+
+func TestStart(t *testing.T) {
+	s := server.NewServer(testConfig)
+	serverAddr, err := s.Listen("localhost:0")
+	require.NoError(t, err)
+	go func() {
+		s.Launch()
+		t.Log("Server stopped.")
+	}()
+
+	client1 := server.NewSampleClient()
+	err = client1.Connect(serverAddr)
+	require.NoError(t, err)
+	client2 := server.NewSampleClient()
+	err = client2.Connect(serverAddr)
+	require.NoError(t, err)
+	client3 := server.NewSampleClient()
+	err = client3.Connect(serverAddr)
+	require.NoError(t, err)
+
+	joinRes1, err := client1.JoinGame()
+	require.NoError(t, err)
+	joinRes2, err := client2.JoinGame()
+	require.NoError(t, err)
+	require.Equal(t, joinRes1.GameId, joinRes2.GameId)
+
+	err = client2.StartGame()
+	require.NoError(t, err)
+
+	joinRes3, err := client3.JoinGame()
+	require.NoError(t, err)
+	// check that after starting, next user joins into different game.
+	// test passes, however, it may happen that client3.JoinGame is handled
+	// before client2.StartGame. In that case, the test may fail.
+	require.NotEqual(t, joinRes1.GameId, joinRes3.GameId)
 }
 
 func TestCredit(t *testing.T) {
@@ -97,6 +138,10 @@ func TestCredit(t *testing.T) {
 
 	err = client.StartGame()
 	require.NoError(t, err)
+
+	// this is needed, since after start is handled, the stream
+	// goroutine will be abruptly finished. so I'm giving it time
+	// to process events.
 	time.Sleep(2 * time.Second)
 
 	// TODO: finish it. I think I'll need
